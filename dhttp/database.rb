64bb1666780577578446b6f223b545fd287f507f
Jesse Gallagher <jesse@secondfoundation.org>
2013-03-02 16:25:03 -0500

    Added some argument checking and a TODO

--- dhttp/database.rb
+++ dhttp/database.rb
@@ -3,8 +3,10 @@ module DHTTP
     attr_reader :server, :path, :username, :password, :port, :ssl
     
     def initialize(args)
-      @server = args[:server]
-      @path = args[:path]
+      raise TypeError.new("Args must be a hash") if not args.is_a? Hash
+      
+      @server = args[:server] or raise "Missing :server parameter"
+      @path = args[:path] or raise "Missing :path parameter"
       @username = args[:username] || ""
       @password = args[:password] || ""
       @ssl = args[:ssl] or false
@@ -23,17 +25,18 @@ module DHTTP
     def to_s
       {
         :server => @server,
-        :path => @path,
-        :username => @username,
-        :password => @password
+        :path => @path
       }.inspect
     end
+    
+    
     
     def fetch(path)
       uri = URI("http#{@ssl ? 's' : ''}://#{@server}:#{@port}/#{@path}/api/data#{path}")
       http = Net::HTTP.new(uri.host, uri.port)
       req = Net::HTTP::Get.new(uri.request_uri)
       unless @username.empty?
+        # TODO: make this actually work
         req.basic_auth @username, @password
       end
       http.request(req)
